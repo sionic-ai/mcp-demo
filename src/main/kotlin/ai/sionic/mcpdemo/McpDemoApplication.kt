dc52bc63b6ff746aca8abece8b3ea544894c7aad
Hyune-c <choi8608@gmail.com>
2025-03-19 16:57:11 +0900

    project init

---
+++ src/main/kotlin/ai/sionic/mcpdemo/McpDemoApplication.kt
@@ -0,0 +1,11 @@
+package ai.sionic.mcpdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class McpDemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<McpDemoApplication>(*args)
+}
---
+++ src/test/kotlin/ai/sionic/mcpdemo/McpDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package ai.sionic.mcpdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class McpDemoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
